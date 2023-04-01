210bcc2353ea24a28524087eb0de71f70f6148f6
mikhail <mikhail.shirokov@phystech.edu>
2023-04-02 00:29:10 +0700

    rename service

--- src/hello_test.cpp
+++ src/hello_test.cpp
@@ -3,8 +3,8 @@
 #include <userver/utest/utest.hpp>
 
 UTEST(SayHelloTo, Basic) {
-  using pg_grpc_service_template::SayHelloTo;
-  using pg_grpc_service_template::UserType;
+  using tinkoff_api_userver::SayHelloTo;
+  using tinkoff_api_userver::UserType;
 
   EXPECT_EQ(SayHelloTo("Developer", UserType::kFirstTime),
             "Hello, Developer!\n");
